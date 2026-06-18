10c9a0cb1b59920c99d41742ec7cbdb6d1b2daf8
jeffgat <jeff.gtbn@gmail.com>
2026-06-18 12:20:45 -0600

    chore: initial commit

---
+++ apps/api/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/health', (_, res) => {
+    res.json({ status: 'ok' });
+});
+
+app.listen(3000, () => {
+  console.log('api running on :3000');
+})
